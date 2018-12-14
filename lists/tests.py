d0a75a0acf3687d360341af001b002514c09986a
bluelimelearning247 <bluelimelearning247@gmail.com>
2018-12-14 20:37:06 +0000

    First unit test pass and url mapping ,dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
 # self represents the instance of the class.
 # By using self keyword we can access the attribute
 #and methods of the class in python.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-	def test_bad_addition(self):
-		self.assertEqual(7 + 7 , 13)
+	def test_root_url_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
